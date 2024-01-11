72b13055ee6ce284233c4d7244f00ff27c4b8601
rtarun9 <rtarun0913@gmail.com>
2024-01-11 21:07:56 +0530

    Added solution for day 9 part 1 and 2

---
+++ src/day-9/part_1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "../common/file_utils.hpp"
+
+int main()
+{
+    const auto file_res = common::read_file_by_line("input.txt");
+    if (!file_res.has_value())
+    {
+        return EXIT_FAILURE;
+    }
+
+    int result = 0;
+    for (const auto& line : file_res.value())
+    {
+        const auto history = common::split_string_by_space(line);
+        std::vector<int> numbers = {};
+
+        for (const auto& hist : history)
+        {
+            numbers.emplace_back(std::stoi(hist));
+        }
+
+        // Brute force solution:
+        std::vector<std::vector<int>> history_sequences{};
+        history_sequences.push_back(numbers);
+
+        const auto reached_final_sequence = [&](const auto history_sequences)
+        {
+            if (history_sequences.size() == 0)
+            {
+                return false;
+            }
+
+            for (const auto& num : history_sequences)
+            {
+                if (num != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+        while (!reached_final_sequence(history_sequences.back()))
+        {
+            std::vector<int> num{};
+            for (int i = 0; i < history_sequences.back().size() - 1; i++)
+            {
+                num.push_back(history_sequences.back()[i + 1] - history_sequences.back()[i]);
+            }
+
+            history_sequences.emplace_back(num);
+        }
+
+        // Now, find the solution.
+        int last_value_in_row = 0;
+        for (int i = history_sequences.size() - 1; i >= 0; --i)
+        {
+            last_value_in_row = last_value_in_row + history_sequences[i].back();
+        }
+
+        result += last_value_in_row;
+    }
+
+    std::cout << "Result :: " << result << "\n";
+
+    return EXIT_SUCCESS;
+} 
